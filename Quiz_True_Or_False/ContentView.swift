a922e6975fd48fd6a609f39ce2b088513ee8284d
alexmortelsison <alexmortelsison@gmail.com>
2024-03-25 20:36:48 +0900

    Initial Commit

---
+++ Quiz_True_Or_False/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Quiz_True_Or_False
+//
+//  Created by Alex Sison on 3/25/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Quiz_True_Or_False/Quiz_True_Or_FalseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Quiz_True_Or_FalseApp.swift
+//  Quiz_True_Or_False
+//
+//  Created by Alex Sison on 3/25/24.
+//
+
+import SwiftUI
+
+@main
+struct Quiz_True_Or_FalseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
